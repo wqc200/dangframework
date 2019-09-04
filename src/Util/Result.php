e6faabc2af867e0a1b71a6bf37abb4a6d14ce24f
wqc200 <wqc200@gmail.com>
2019-09-04 21:57:10 +0800

    mvc

---
+++ src/Util/Result.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dang\Util;
+
+
+class Result
+{
+    private $_errorCode;
+    private $_message;
+    private $_data;
+
+    function __construct()
+    {
+    }
+
+    function setErrorCode($errorCode){
+        $this->_errorCode = $errorCode;
+    }
+
+    function getErrorCode(){
+        return $this->_errorCode;
+    }
+
+    function setMessage($message){
+        $this->_message = $message;
+    }
+
+    function getMessage(){
+        return $this->_message;
+    }
+
+    function setData($data){
+        $this->_data = $data;
+    }
+
+    function getData(){
+        return $this->_data;
+    }
+}
